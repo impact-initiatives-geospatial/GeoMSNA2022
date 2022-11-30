63658dc092e77a7776b5437b85190f4608c345c5
Anthony Twesigye <twesigye10@gmail.com>
2022-11-30 15:07:49 +0300

    functions for creating some scatter plots. Data aggregated on aparticular column

---
+++ R/hex_plot.R
@@ -0,0 +1,49 @@
+#' hex_plot_mean_rs_mean_msna_ind
+#' Creates a scaterplot of rs indicators added to the data and other numeric indicators
+#' @param input_dataset 
+#' @param group_by_col 
+#' @param input_x_col_rs 
+#' @param input_y_col 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+hex_plot_mean_rs_mean_msna_ind <- function(input_dataset, group_by_col, 
+                                           input_x_col_rs, input_y_col) {
+  df_data_process <- input_dataset |> 
+    group_by({{group_by_col}}) |> 
+    summarise({{input_x_col_rs}} := mean(!!sym(input_x_col_rs), na.rm = TRUE),
+              {{input_y_col}} := mean(!!sym(input_y_col), na.rm = TRUE))
+  
+  ggplot2::ggplot(data = df_data_process, aes(x = !!sym(input_x_col_rs), y = !!sym(input_y_col))) +
+    ggplot2::geom_point() +
+    ggplot2::geom_smooth(method=lm) +
+    ggpubr::stat_cor(method = "pearson", label.x = 0, label.y = 1)
+}
+
+#' hex_plot_mean_rs_count_msna_ind
+#' Creates a scaterplot of rs indicators added to the data and other categorical indicators
+#' @param input_dataset 
+#' @param group_by_col 
+#' @param input_x_col_rs 
+#' @param input_y_col 
+#' @param search_y_col 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+hex_plot_mean_rs_count_msna_ind <- function(input_dataset, group_by_col, 
+                                            input_x_col_rs, input_y_col, 
+                                            search_y_col = c("yes")) {
+  df_data_process <- input_dataset |> 
+    group_by({{group_by_col}}) |> 
+    summarise({{input_x_col_rs}} := mean(!!sym(input_x_col_rs), na.rm = TRUE),
+              {{input_y_col}} := sum(!!sym(input_y_col) %in% search_y_col, na.rm = TRUE))
+  
+  ggplot2::ggplot(data = df_data_process, aes(x = !!sym(input_x_col_rs), y = !!sym(input_y_col))) +
+    ggplot2::geom_point() +
+    ggplot2::geom_smooth(method=lm) +
+    ggpubr::stat_cor(method = "pearson", label.x = 0, label.y = 1)
+}
