b12584fbb19bf77a2268002d91e7ea8e8d2bc220
Anthony Twesigye <twesigye10@gmail.com>
2022-12-01 09:46:48 +0300

    updated plot functions with title

--- R/hex_plot.R
+++ R/hex_plot.R
@@ -19,7 +19,8 @@ hex_plot_mean_rs_mean_msna_ind <- function(input_dataset, group_by_col,
   ggplot2::ggplot(data = df_data_process, aes(x = !!sym(input_x_col_rs), y = !!sym(input_y_col))) +
     ggplot2::geom_point() +
     ggplot2::geom_smooth(method=lm) +
-    ggpubr::stat_cor(method = "pearson", label.x = 0, label.y = 1)
+    ggpubr::stat_cor(method = "pearson", label.x = 0, label.y = 1) +
+    ggplot2::labs(title = paste("Plot of", input_x_col_rs ,"and", input_y_col))
 }
 
 #' hex_plot_mean_rs_count_msna_ind
@@ -45,5 +46,6 @@ hex_plot_mean_rs_count_msna_ind <- function(input_dataset, group_by_col,
   ggplot2::ggplot(data = df_data_process, aes(x = !!sym(input_x_col_rs), y = !!sym(input_y_col))) +
     ggplot2::geom_point() +
     ggplot2::geom_smooth(method=lm) +
-    ggpubr::stat_cor(method = "pearson", label.x = 0, label.y = 1)
+    ggpubr::stat_cor(method = "pearson", label.x = 0, label.y = 1) +
+    ggplot2::labs(title = paste("Plot of", input_x_col_rs ,"and", input_y_col))
 }
