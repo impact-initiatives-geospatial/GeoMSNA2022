4657af102e333c26f0e25b7b5e9727dd87abd1c2
zackarno <zackarno@gmail.com>
2022-12-05 16:22:53 -0500

    add `input_dir()` to `load_hh_data()` as interim method

--- R/input_dir.R
+++ R/input_dir.R
@@ -9,9 +9,9 @@
 #' @examples
 
 input_dir <-  function(env_var= "MSNA2022_DIR" ,country_code){
-  if(country_code=="col"){
+  # if(country_code=="col"){
     res_fp <- file.path(Sys.getenv(env_var),country_code,"inputs")
-  }
+  # }
   return(res_fp)
   
 }
@@ -30,5 +30,34 @@ output_dir <-  function(env_var= "MSNA2022_DIR" ,country_code){
     res_fp <- file.path(Sys.getenv(env_var),country_code,"outputs")
   }
   return(res_fp)
+  
+}
+
+
+
+
+#' load_hh_data
+#' @param env_var 
+#' @param country_code 
+#'
+#' @return data.frame containing hh data
+#' @export
+#'
+#' @examples \dontrun{
+#' library(GeoMSNAs2022)
+#' load_hh_data("nga")
+#' }
+
+load_hh_data <-  function(env_var= "MSNA2022_DIR" ,country_code){
+  if(country_code=="nga"){
+    dat_fp <- file.path(input_dir(env_var = "MSNA2022_DIR",country_code = country_code), "20221107_msna_with_rs_nga.rds")
+    res <- readr::read_rds(dat_fp)  
+  }
+  if(country_code=="irq"){
+    dat_fp <- file.path(input_dir(env_var = "MSNA2022_DIR",country_code = country_code), "irq_msna_clean_data_with_rs_indicators.csv")
+    
+    res <- readr::read_csv(dat_fp)  
+  }
+  return(res)
   
 }
